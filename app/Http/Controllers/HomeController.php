64af0339605dfb089e268231ac511e26fd39afd0
ibnukr <2041720159@student.polinema.ac.id>
2022-07-01 14:50:59 +0700

    Jobsheet 6 Practicum 2: Add login as Administrator

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+        return view('home');
+    }
+
+    public function main()
+    {
         return view('page.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -77,11 +77,14 @@ use App\Http\Controllers\ArticlesController;
 // Route::get('/about-us', function(){
 //     return redirect('https://www.educastudio.com/about-us');
 // });
-// Auth::routes();
+Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/about', [App\Http\Controllers\AboutController::class, 'index'])->name('about');
 Route::get('/contact', [App\Http\Controllers\ContactController::class, 'index'])->name('contact');
+
+
 
 
 
